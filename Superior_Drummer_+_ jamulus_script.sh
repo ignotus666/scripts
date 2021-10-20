8f64d423fe3b1c86859d864d67027599957b4b2f
ignotus <darylo1@hotmail.com>
2021-10-20 17:15:38 +0200

    Add files via upload

---
+++ Superior_Drummer_+_ jamulus_script.sh
@@ -0,0 +1,29 @@
+﻿#!/bin/sh
+
+/usr/bin/qjackctl --preset=superior_drummer --active-patchbay=/home/daryl/jack_patchbays/jamulus+superior.xml &
+
+env WINEPREFIX="/home/daryl/.wine" /opt/wine-stable/bin/wine C:\\Program\ Files\\Toontrack\\Superior\ Drummer\\Superior\ Drummer\ 3.exe   &
+
+sleep 2
+
+jamulus -s -F &
+
+#sleep 2
+
+jamulus --ctrlmidich '1;f30*10;p80*10;s2*3;m5*3' &
+
+#sleep 2
+
+jack_mixer --config=/home/daryl/jack_mixer_configs/superior &
+
+sleep 40
+
+jack_disconnect 'Superior Drummer 3:out_1' system:playback_1
+jack_disconnect 'Superior Drummer 3:out_2' system:playback_2
+jack_disconnect system:capture_1 'Superior Drummer 3:in_1'
+jack_disconnect system:capture_2 'Superior Drummer 3:in_2'
+jack_disconnect system:capture_1 'Jamulus:input left'
+jack_disconnect system:capture_2 'Jamulus:input right'
+jack_disconnect 'Jamulus:output left' system:playback_1
+jack_disconnect 'Jamulus:output right' system:playback_2
+
