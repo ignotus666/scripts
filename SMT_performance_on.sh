029f02a3636d6cc5e071e8a488a90b728f2da081
Ignotus <darylo1@hotmail.com>
2021-10-21 08:08:35 +0200

    add scripts

---
+++ SMT_performance_on.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/bash
+
+if ! pgrep -x a2jamidid >/dev/null
+then
+    cpupower-gui -p
+
+        if ! pgrep -x VirtualBoxVM >/dev/null
+        then
+        sudo '/home/daryl/scripts/smt-manager.pl' --offline
+        fi
+fi
